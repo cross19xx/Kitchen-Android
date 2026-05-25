3810c7a8d5bc328aefe9989a5c7a32fc49118f2b
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2026-05-25 12:51:01 +0000

    switch to local fonts instead of Google fonts

--- film-nest-android/app/build.gradle.kts
+++ film-nest-android/app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     // Testing
--- film-nest-android/app/src/main/java/com/cross19xx/filmnest/core/theme/Typography.kt
+++ film-nest-android/app/src/main/java/com/cross19xx/filmnest/core/theme/Typography.kt
@@ -1,30 +1,50 @@
 package com.cross19xx.filmnest.core.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import com.cross19xx.filmnest.R
 
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs,
+var bodyFontFamily = FontFamily(
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_thin_italic, FontWeight.Thin, style = FontStyle.Italic),
+    Font(R.font.inter_extralight, FontWeight.ExtraLight),
+    Font(R.font.inter_extralight_italic, FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_light_italic, FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_italic, FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_medium_italic, FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_semibold_italic, FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_bold_italic, FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.inter_extrabold, FontWeight.ExtraBold),
+    Font(R.font.inter_extrabold_italic, FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(R.font.inter_black, FontWeight.Black),
+    Font(R.font.inter_black_italic, FontWeight.Black, style = FontStyle.Italic),
 )
 
-val bodyFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Inter"),
-        fontProvider = provider,
-    )
+var displayFontFamily = FontFamily(
+    Font(R.font.ibm_plex_serif_thin, FontWeight.Thin),
+    Font(R.font.ibm_plex_serif_thin_italic, FontWeight.Thin, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_extralight, FontWeight.ExtraLight),
+    Font(R.font.ibm_plex_serif_extralight_italic, FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_light, FontWeight.Light),
+    Font(R.font.ibm_plex_serif_light_italic, FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_regular, FontWeight.Normal),
+    Font(R.font.ibm_plex_serif_italic, FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_medium, FontWeight.Medium),
+    Font(R.font.ibm_plex_serif_medium_italic, FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_semibold, FontWeight.SemiBold),
+    Font(R.font.ibm_plex_serif_semibold_italic, FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(R.font.ibm_plex_serif_bold, FontWeight.Bold),
+    Font(R.font.ibm_plex_serif_bold_italic, FontWeight.Bold, style = FontStyle.Italic),
 )
 
-val displayFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("IBM Plex Serif"),
-        fontProvider = provider,
-    )
-)
 
 val baseline = Typography()
 
