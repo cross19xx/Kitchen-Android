bc48363632bfa17a6738f5ba5718ce9566d18cf6
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2026-06-02 15:02:20 +0000

    feat: wire MediaPipe embedder, Room database, and app scope into Hilt

--- film-nest-android/app/src/main/java/com/cross19xx/filmnest/data/local/FilmNestDatabase.kt
+++ film-nest-android/app/src/main/java/com/cross19xx/filmnest/data/local/FilmNestDatabase.kt
@@ -4,6 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
+const val FILM_NEST_DB_NAME = "filmnest.db"
+
 @Database(entities = [MediaEmbeddingEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class FilmNestDatabase : RoomDatabase() {
